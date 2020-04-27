129d62fcb976a1b08364de72e9781123f01f5100
Enderlook <enderlook@gmail.com>
2020-04-27 15:29:12 -0300

    Fix error message of MemberNotFoundException

--- Enderlook.Extensions/code/Reflection/Exceptions/MemberNotFoundException.cs
+++ Enderlook.Extensions/code/Reflection/Exceptions/MemberNotFoundException.cs
@@ -5,6 +5,6 @@ namespace Enderlook.Extensions
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "<pendiente>")]
     public class MemberNotFoundException : Exception
     {
-        public MemberNotFoundException(string memberName, Type type) : base($"No member named {memberName} not found in {nameof(Type)} {type}.") { }
+        public MemberNotFoundException(string memberName, Type type) : base($"No member named {memberName} was found in {nameof(Type)} {type}.") { }
     }
 }
